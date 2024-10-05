ba6ba43baab1d5359fc8c19e01f8d1e03018a1d8
Wendel Davi <149116208+Wendel-Costa@users.noreply.github.com>
2024-10-05 15:02:57 -0300

    criando crud básico do user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -7,7 +7,46 @@ class UserController {
             const listaUsers = await user.find({});
             res.status(200).json(listaUsers);
         } catch (erro) {
-            res.status(500).json({ message: `${erro.message} - falha ao listar usuário` });
+            res.status(500).json({ message: `${erro.message} - falha ao listar usuários` });
+        }
+    }
+
+    static async listarUserPorId(req, res) {
+        try {
+            const id = req.params.id;
+            const userEncontrado = await user.findById(id);
+            res.status(200).json(userEncontrado);
+        } catch (erro) {
+            res.status(500).json({ message: `${erro.message} - falha na requisição do usuário` });
+        }
+    }
+
+    static async cadastrarUser(req, res) {
+        try {
+            const novoUser = await user.create(req.body);
+            res.status(201).json({message: "Usuário criado com sucesso", usuario: novoUser});
+        } catch (erro) {
+            res.status(500).json({ message: `${erro.message} - falha ao cadastrar usuário` });
+        }
+    }
+
+    static async atualizarUser(req, res) {
+        try {
+            const id = req.params.id;
+            await user.findByIdAndUpdate(id, req.body);
+            res.status(200).json({message: "Usuário atualizado com sucesso"});
+        } catch (erro) {
+            res.status(500).json({ message: `${erro.message} - falha na atualização do usuário` });
+        }
+    }
+
+    static async excluirUser(req, res) {
+        try {
+            const id = req.params.id;
+            await user.findByIdAndDelete(id);
+            res.status(200).json({message: "Usuário excluído com sucesso"});
+        } catch (erro) {
+            res.status(500).json({ message: `${erro.message} - falha na exclusão do usuário` });
         }
     }
 }
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,5 +4,9 @@ import UserController from "../controllers/userController.js";
 const routes = express.Router();
 
 routes.get("/users", UserController.listarUsers);
+routes.get("/users/:id", UserController.listarUserPorId);
+routes.post("/users", UserController.cadastrarUser);
+routes.put("/users/:id", UserController.atualizarUser);
+routes.delete("/users/:id", UserController.excluirUser);
 
 export default routes;
