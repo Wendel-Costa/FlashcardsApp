62cc19d6676388c6902b9acb3db24fe32c9bf0eb
Wendel Davi <149116208+Wendel-Costa@users.noreply.github.com>
2024-11-03 13:28:30 -0300

    Iniciando frontend (formulario básico)

---
+++ public/script.js
@@ -0,0 +1,8 @@
+function gerarFlashcards(){
+    const topico = document.querySelector("#topico").value;
+    const detalhe = document.querySelector('input[name="nivelDetalhe"]:checked').value;
+
+    console.log(topico);
+    console.log(detalhe);
+    alert("Ok");
+}
