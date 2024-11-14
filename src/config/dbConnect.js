1f1cb93e08e171c91bbdc717ed3584e8755e4182
Wendel Davi <149116208+Wendel-Costa@users.noreply.github.com>
2024-11-14 20:50:52 -0300

    tratamento de erros na conexão

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -2,8 +2,15 @@ import "dotenv/config"
 import mongoose from "mongoose";
 
 async function conectarNaDatabase() {
-    mongoose.connect(process.env.DB_CONEXAO_STRING);
-
+    try {
+        await mongoose.connect(process.env.DB_CONEXAO_STRING, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("Conectado ao MongoDB!");
+    } catch (error) {
+        console.error("Erro ao conectar ao MongoDB:", error);
+    }
     return mongoose.connection;
 }
 
