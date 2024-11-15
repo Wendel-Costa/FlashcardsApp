aa3b52ddb6b97069e8acd70242af02ce3078daf4
Wendel Davi <149116208+Wendel-Costa@users.noreply.github.com>
2024-11-14 21:04:13 -0300

    testando variaveis ambientes

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,11 +1,14 @@
 import "dotenv/config"
 import mongoose from "mongoose";
 
+console.log("DB_CONEXAO_STRING:", process.env.DB_CONEXAO_STRING);
+
 async function conectarNaDatabase() {
     try {
         await mongoose.connect(process.env.DB_CONEXAO_STRING, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 10000,
         });
         console.log("Conectado ao MongoDB!");
     } catch (error) {
