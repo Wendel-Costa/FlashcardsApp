d02226112b4c421f6f9649adf367253e2b5557a0
Wendel Davi <149116208+Wendel-Costa@users.noreply.github.com>
2025-01-18 17:27:56 -0300

    adicionando o fecht do banco de cards

--- public/script.js
+++ public/script.js
@@ -1,11 +1,32 @@
-const URL = 'https://apiflashcards.vercel.app';
+const URL = 'https://apiflashcards.vercel.app/cards';
 
 async function chamarApi() {
     const resp = await fetch(URL);
+    let cards;
     if (resp.status === 200){
-        const cards = await resp.json();
+        cards = await resp.json();
         console.log(cards);
+        exibirFlashcards(cards);
     }
+
+}
+
+function exibirFlashcards(cards) {
+    const outputSection = document.querySelector('.main_output');
+    outputSection.innerHTML = ''; // Limpa o conteúdo anterior
+
+    cards.forEach(card => {
+        const cardElement = document.createElement('div');
+        cardElement.classList.add('flashcard'); // Adicione uma classe para estilizar
+
+        cardElement.innerHTML = `
+            <h3>${card.pergunta}</h3>
+            <p><strong>Resposta:</strong> ${card.resposta}</p>
+            <p><strong>Tag:</strong> ${card.tag}</p>
+        `;
+
+        outputSection.appendChild(cardElement);
+    });
 }
 
 document.querySelector('#botaoGerarCard').addEventListener('click', () => chamarApi());
