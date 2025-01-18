19060c69ab27417ac6fcd7715e09d9b52387cbe7
Wendel Davi <149116208+Wendel-Costa@users.noreply.github.com>
2025-01-18 19:49:19 -0300

    teste de novo

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,7 @@ import cards from "./cardRoutes.js";
 const routes = (app) => {
     app.route("/").get((req,res) => res.status(200).send("App de flahscards 100% atualizado"))
 
-    app.use(express.json(), users, cards);
+    app.use("/api", express.json(), users, cards);
 }
 
 export default routes;
