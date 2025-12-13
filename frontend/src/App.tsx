545d0ae6ef47e3e27384331f1bffc94f340277ee
Wendel Davi <149116208+Wendel-Costa@users.noreply.github.com>
2025-12-13 16:49:15 +0000

    creating container component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
 import './styles/theme.css';
 import './styles/global.css';
 import { Footer } from './components/Footer';
+import { Container } from './components/Container';
 
 function App() {
   return (
     <>
-      <p>Oi</p>
-      <Footer />
+      <Container>Oi</Container>
+
+      <Container>
+        <Footer />
+      </Container>
     </>
   )
 }
