b3c67b1a8762389100b6a93a9efaf99b558a5cf5
Wendel Davi <149116208+Wendel-Costa@users.noreply.github.com>
2025-12-14 16:19:29 +0000

    styling HomeHeader

--- frontend/src/components/HomeHeader/index.tsx
+++ frontend/src/components/HomeHeader/index.tsx
@@ -1,3 +1,4 @@
+import { LogOut, Plus, Sparkles } from 'lucide-react'
 import { DefaultHeader } from '../DefaultHeader'
 import styles from './styles.module.css'
 
@@ -5,9 +6,20 @@ export function HomeHeader() {
    return (
       <DefaultHeader>
          <div className={styles.homeHeader}>
-            <a href="">Criar Card</a>
-            <a href="">Criar Card Com IA</a>
-            <a href="">Sair</a>
+            <a href="" className={styles.buttonCard}>
+               <Plus />
+               <p>Criar Card</p>
+            </a>
+
+            <a href="" className={styles.buttonCardAI}>
+               <Sparkles />
+               <p>Criar Card Com IA</p>
+            </a>
+
+            <a href="" className={styles.buttonOut}>
+               <LogOut />
+               <p>Sair</p>
+            </a>
          </div>
       </DefaultHeader>
    )
