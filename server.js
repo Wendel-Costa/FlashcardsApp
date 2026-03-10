75738093153b2ffdb4c844d093ecebeabe5bc7c0
Wendel-Costa <wendeldavirc@gmail.com>
2026-03-10 15:55:17 -0300

    fixing app.listen

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import app from './backend/app.js';
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-   console.log(`Server listening on port ${port}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+   const port = process.env.PORT || 3000;
+   app.listen(port, () => {
+      console.log(`Server listening on port ${port}`);
+   });
+}
 
 export default app;
